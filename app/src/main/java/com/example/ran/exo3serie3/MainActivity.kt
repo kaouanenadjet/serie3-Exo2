d7e51e40fdedc577c3354289b49424c1aebe6839
BouguesriRania <hr_bouguesri@esi.dz>
2021-04-19 22:32:11 +0100

    correction

--- app/src/main/java/com/example/ran/exo3serie3/MainActivity.kt
+++ app/src/main/java/com/example/ran/exo3serie3/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.ran.exo3serie3
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.content.res.Configuration
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+            super.onConfigurationChanged(newConfig)
+        // use your setcontentView here
+            setContentView(R.layout.activity_main_other)
 
     }
 }
