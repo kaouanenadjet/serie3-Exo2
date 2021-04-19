fa2df8f5514587901817f12c817ec4bf6d42bbcb
BouguesriRania <hr_bouguesri@esi.dz>
2021-04-19 22:58:54 +0100

    correction2

--- app/src/main/java/com/example/ran/exo3serie3/MainActivity.kt
+++ app/src/main/java/com/example/ran/exo3serie3/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.ran.exo3serie3
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.res.Configuration
+import android.widget.Button
+import android.support.v4.content.ContextCompat
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +15,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
+            var btn: Button? = null
             super.onConfigurationChanged(newConfig)
         // use your setcontentView here
-            setContentView(R.layout.activity_main_other)
+        btn=findViewById<Button>(R.id.button5)
+        btn.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.red))
+        setContentView(R.layout.activity_main)
 
     }
 }
